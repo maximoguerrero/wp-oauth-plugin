8d9e72f84c12bb4aa6c51f720a9fbe873a0008f9
Maximo G <maximog@mit.edu>
2025-03-31 19:08:32 -0400

    Refactor OAuth2 SSO functions for improved readability and maintainability

--- oauth2-sso.php
+++ oauth2-sso.php
@@ -43,7 +43,7 @@ function oauth2_sso_init()
     if (is_plugin_active(plugin_basename(OAUTH2_SSO_PLUGIN_DIR . 'oauth2-sso.php'))) {
         // Check if the user is attempting to log in via OAuth2.
         if (isset($_GET['oauth2_sso']) || isset($_GET['code']) || isset($_GET['error'])) {
-            
+
             // set no cache headers
             header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
             header("Cache-Control: post-check=0, pre-check=0", false);
@@ -52,7 +52,7 @@ function oauth2_sso_init()
             // Handle the login process.
             oauth2_sso_handle_login();
         }
-        
+
         if (is_front_page() || is_home()) {
             // Set no cache headers for the root website.
             header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
@@ -60,54 +60,63 @@ function oauth2_sso_init()
             header("Pragma: no-cache");
         }
     }
-
 }
 
-function require_authentication() {
-    if ( ! is_user_logged_in() ) {
+function require_authentication()
+{
+    // Ignore requests to wp-cron.php.
+    if (strpos($_SERVER['REQUEST_URI'], 'wp-cron.php') !== false) {
+        return;
+    }
+    if (! is_user_logged_in()) {
+
+       
+
         // redirecto to touchstone directly.
-        $redirect_uri = get_option( 'oauth2_sso_redirect_uri' );
-        if ( $redirect_uri && ! empty( $redirect_uri ) ) {
+        $redirect_uri = get_option('oauth2_sso_redirect_uri');
+        if ($redirect_uri && ! empty($redirect_uri)) {
             header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
             header("Cache-Control: post-check=0, pre-check=0", false);
             header("Pragma: no-cache");
-            wp_redirect( $redirect_uri . '?oauth2_sso=true&redirect_uri=' . esc_url( get_current_url() ) );
+            wp_redirect($redirect_uri . '?oauth2_sso=true&redirect_uri=' . esc_url(get_current_url()));
             exit;
         }
-        wp_redirect( wp_login_url( get_current_url() ) );
+        wp_redirect(wp_login_url(get_current_url()));
         exit;
     }
 }
-function get_current_url() {
-    $url = ( isset( $_SERVER['HTTPS'] ) && 'on' === $_SERVER['HTTPS'] ? 'https' : 'http' )
-    . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+function get_current_url()
+{
+    $url = (isset($_SERVER['HTTPS']) && 'on' === $_SERVER['HTTPS'] ? 'https' : 'http')
+        . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     $url = strtok($url, '?');
-    
+
     // Add query parameters if they exist.
     if (!empty($_GET)) {
         $query_params = http_build_query($_GET);
         $url .= '?' . $query_params;
     }
-    
+
 
     return $url;
-}  
+}
 
 // Add a button to the login form.
-function oauth2_sso_add_login_button() {
-    
-    
-    // Save the 'redirect_to' query string to a variable if it exists.
-    $redirect_to = isset($_GET['redirect_to']) ? "&redirect_uri=". esc_url($_GET['redirect_to']) : '';
+function oauth2_sso_add_login_button()
+{
 
-    
+
+    // Save the 'redirect_to' query string to a variable if it exists.
+    $redirect_to = isset($_GET['redirect_to']) ? "&redirect_uri=" . esc_url($_GET['redirect_to']) : '';
+
+
     $login_text = get_option('oauth2_sso_oauth_login_text');
     if (empty($login_text)) {
         $login_text = 'Login with OAuth2';
     }
 
     // Add a button to the login form.
-    $button_html = '<div id="oauth2SSO" style="margin:20px; clear:both; text-align:center;"><a href="?oauth2_sso='.uniqid().$redirect_to.'" class="button button button-large">'. $login_text .'</a></div>';
+    $button_html = '<div id="oauth2SSO" style="margin:20px; clear:both; text-align:center;"><a href="?oauth2_sso=' . uniqid() . $redirect_to . '" class="button button button-large">' . $login_text . '</a></div>';
     echo $button_html;
 }
 
@@ -116,7 +125,8 @@ add_action('login_form', 'oauth2_sso_add_login_button');
 
 
 // Add custom JavaScript to the login page.
-function oauth2_sso_login_js() {
+function oauth2_sso_login_js()
+{
     // Add the JavaScript to move the button to the form.
     echo '
     <script type="text/javascript">
@@ -143,7 +153,8 @@ function oauth2_sso_admin_menu()
 }
 
 // Clear cookie on logout.
-function oauth2_sso_clear_cookie() {
+function oauth2_sso_clear_cookie()
+{
     setcookie('oauth2state', '', time() - 3600, '/');
     clear_oauth_user_cookie();
 }
